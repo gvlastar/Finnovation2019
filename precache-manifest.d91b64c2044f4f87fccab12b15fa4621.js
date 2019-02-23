e9d686b58daf8ff24c6159802167e34a804fa298
Sashank Gogula <sashankg@gmail.com>
2019-02-23 12:54:40 -0500

    Initial gh-pages

---
+++ precache-manifest.d91b64c2044f4f87fccab12b15fa4621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c8bce4aade1e19599c6c45f8090cb5",
+    "url": "/static/media/trump.f5c8bce4.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "910d45e7b5faa3194136",
+    "url": "/static/js/main.910d45e7.chunk.js"
+  },
+  {
+    "revision": "e466296523e8b1329f24",
+    "url": "/static/js/2.e4662965.chunk.js"
+  },
+  {
+    "revision": "910d45e7b5faa3194136",
+    "url": "/static/css/main.9219f989.chunk.css"
+  },
+  {
+    "revision": "833be0868aa664946cae2c45c6bdce98",
+    "url": "/index.html"
+  }
+];
